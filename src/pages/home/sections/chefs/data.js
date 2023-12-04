4339739778ae6cff8b3587eb2b7888aef835f43e
PokizaSdv <pokizasdv@gmail.com>
2023-12-04 04:18:38 -0500

    Make Chefs Section Component Reusable

---
+++ src/pages/home/sections/chefs/data.js
@@ -0,0 +1,23 @@
+import "./Chefs.css"
+
+import betran from "../../../../assets/betran.png";
+import ferry from "../../../../assets/ferry.png";
+import iswan from "../../../../assets/iswan.png"
+
+export const chefs = [
+    {
+        img: betran,
+        name: "Betran Komar",
+        position: "Head chef"
+    },
+    {
+        img: ferry,
+        name: "Ferry Sauwi",
+        position: "Chef"
+    },
+    {
+        img: iswan,
+        name: "Iswan Dracho",
+        position: "Chef"
+    }
+]
